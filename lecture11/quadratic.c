4747f7a1d93c8c93e181c45435ffb7690e454049
Zhao Li <zhaol@hawaii.edu>
2013-09-22 05:15:52 -0400

    add lecture 11

---
+++ lecture11/quadratic.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+main () {
+  float a;
+  float b;
+  float c;
+  float result;
+  
+  float intermediate1;
+  
+  printf("enter in numbers\n");
+  scanf("%f %f %f", &a, &b, &c);
+  intermediate1 = (-b + b*b-4*a*c);
+  intermediate2 = (2*a)
+  result = intermediate1/intermediate2;
+  printf("%f\n", result);
+}
