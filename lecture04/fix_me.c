8c23e8434c5b394ab47480514b81fab9095ba602
Zhao Li <zhaol@hawaii.edu>
2014-09-03 19:28:49 +0000

    lecture04

---
+++ lecture04/fix_me.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+main() {
+
+  int first_number;
+  int second_number;
+  int result;
+  
+  first_number = 3;
+  second_number = 5;
+  
+  printf("first part: %d", first_number);
+  //result = (first_number + second_number) / (second_number - second_number);
+  
+  printf ("The result is: %d \n", result);
+  
+}
