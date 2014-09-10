cb550282502a2f0028fa99df29e8ffe3dfbdcdf0
Zhao Li <zhaol@hawaii.edu>
2014-09-10 18:39:45 +0000

    lecture06

---
+++ lecture06/if.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+main() {
+  
+  int number;
+  
+  printf("Enter a magical number: ");
+  scanf("%d", &number);
+  
+  if (number >= 100) {
+    if (number <= 102) {
+      printf("Your number is very magical! \n");
+    }
+  } else {
+    printf("Your number needs more magic :( \n");
+  }
+}
+
+
+
+/*
+#include <stdio.h>
+
+main() {
+  int number;
+
+  printf ("Enter a magical number: ");
+  scanf ("%d", &number);
+  
+  if (number >= 100) {
+    printf ("Your number is very magical! \n");
+  } else {
+    printf ("Your number needs more magic :( \n");    
+  }
+}
+
+*/
