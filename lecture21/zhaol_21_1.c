1b5946eda95d5bf008c5c9bd44bb8dba8dcfd291
Zhao Li <zhaol@hawaii.edu>
2014-10-24 19:41:43 +0000

    add lecture 22 and 21_1

---
+++ lecture21/zhaol_21_1.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+#define MAX 11
+#define ARRAY_OFFSET -1
+
+int main () {
+  char word[MAX];
+  printf("Please enter in your word:\n");
+  scanf ("%s", word);
+  printf("%s\n", word);
+  
+  int index_to_compare = 1;
+  while (index_to_compare <= MAX) {
+    int index_compared_against = index_to_compare + 1;  
+    printf("letter being compared: %c\n", word[index_to_compare + ARRAY_OFFSET]);
+    while (index_compared_against <= MAX ) {
+      if (word[index_compared_against+ARRAY_OFFSET] >= 97) {
+        if (word[index_compared_against+ARRAY_OFFSET] <= 122) {
+          if (word[index_to_compare+ARRAY_OFFSET] >= word[index_compared_against+ARRAY_OFFSET]) {
+            // SWAP
+            char temp = word[index_to_compare+ARRAY_OFFSET];
+            word[index_to_compare+ARRAY_OFFSET] = word[index_compared_against+ARRAY_OFFSET];
+            word[index_compared_against+ARRAY_OFFSET] = temp;
+            //
+          }
+        }
+      }
+      index_compared_against++;
+    }
+    
+    index_to_compare++;
+  }
+  
+  printf("result: %s\n", word);
+  return 0;
+}
---
+++ lecture22/helper_functions.h
@@ -0,0 +1,7 @@
+float net_balance(float account_balance, float minimum_balance_fee);
+float get_account_balance();
+char get_account_type();
+int is_account_below_minimum_balance(float account_balance, float minimum_balance);
+int ask_to_exit();
+void update_account_summary_information(int *total_number_of_accounts, float *total_amount_in_accounts, float account_balance);
+void get_account_holder(char account_holder[]);
