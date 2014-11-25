29f22a4bdf26fbfef1928edfb6279747ac15f597
Zhao Li <zhaol@hawaii.edu>
2014-11-25 09:25:11 +0000

    adding exam2 ascii_table.c

---
+++ exam2/ascii_table.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main () {
+  char alpha = '1';
+  int number = 1;
+  
+  if(alpha == '1') {
+    printf("yes!\n");
+  } else {
+    printf("no!!!\n");
+  }
+  
+  return 0;
+}
