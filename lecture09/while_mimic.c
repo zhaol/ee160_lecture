27d29ca17fbcb0948abee98e1f39c523b5a1b16d
Zhao Li <zhaol@hawaii.edu>
2015-07-02 17:41:52 +0000

    clearing out for new semester

--- lecture09/while_mimic.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-main() {
-  printf ("Hip, hip, hooray!\n");
-  printf ("Hip, hip, hooray!\n");
-  printf ("Hip, hip, hooray!\n");
-  printf ("That was the last cheer. :(\n");
-}
