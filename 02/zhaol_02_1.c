01744bae14d7256c6e00af84b1c291abd2b583cb
Zhao Li <zhaol@hawaii.edu>
2015-08-27 03:54:10 +0000

    after lecture02

---
+++ 02/zhaol_02_1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+/* this is ignored by the computer; so I can explain what the code does
+  this is a simple example of a C program
+*/
+main() {
+    // this is also ignored by the computer
+   printf("Hello World\n");
+    printf("Bye World\n");
+}
